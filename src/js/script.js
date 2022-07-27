39e6df1e0ec4f8ab7e8cb0a062cb0146a002a6f5
Anatoliy <Astarskiy@yandex.ru>
2022-07-27 16:25:04 +0300

    второй экран

--- src/js/script.js
+++ src/js/script.js
@@ -3,6 +3,7 @@
 const hamburger = document.querySelector('.hamburger');
 const menu = document.querySelector('.menu');
 const closeMenu = document.querySelector('.menu__close');
+const menuLink = document.querySelectorAll('.menu__link');
 
 hamburger.addEventListener('click', () => {
     menu.classList.add('active');
@@ -12,19 +13,20 @@ closeMenu.addEventListener('click', () => {
     menu.classList.remove('active');
 });
 
+
 // buttons 
 
 const btns = document.querySelectorAll('.promo__link');
 
 btns[0].addEventListener('click', () => {
-    if (!btns[0].classList.contains('btn_active')){
+    if (!btns[0].classList.contains('btn_active')) {
         btns[0].classList.add('btn_active');
         btns[1].classList.remove('btn_active');
     }
 });
 
 btns[1].addEventListener('click', () => {
-    if (!btns[1].classList.contains('btn_active')){
+    if (!btns[1].classList.contains('btn_active')) {
         btns[1].classList.add('btn_active');
         btns[0].classList.remove('btn_active');
     }
